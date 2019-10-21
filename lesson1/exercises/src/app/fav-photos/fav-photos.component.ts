c836025fd0df51ef60355cab469f7c8880cd06f1
Eric <eric.earlywine@hotmail.com>
2019-10-21 01:47:55 -0500

    finished exercise

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Favorite photos';
+  photosTitle = 'Some photos';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
   image2 = 'https://i.kym-cdn.com/photos/images/original/001/343/712/058.jpg';
   image3 = '';
