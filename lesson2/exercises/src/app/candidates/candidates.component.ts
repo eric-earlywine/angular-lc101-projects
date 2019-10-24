ea83bdd03c88b1858e6856e0854abebb5710101f
Eric <eric.earlywine@hotmail.com>
2019-10-24 00:44:57 -0500

    Finished lesson 2 exercise

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -24,9 +24,14 @@ export class CandidatesComponent implements OnInit {
   }
 
   // Code the addToCrew function here:
-
+  addToCrew(candidate: object) {
+    if (!this.crew.includes(candidate)) {
+      this.crew.push(candidate);
+    }
+  }
 
   // BONUS: Code the changeMissionName function here:
-
-
+  changeMissionName(newName: string) {
+    this.missionName = newName;
+  }
 }
